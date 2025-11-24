3c45f522ecdbe5dd58d287b78027a1285c0187e5
Sri Medicherla <sri.medicherla@marquette.edu>
2025-11-24 13:05:44 -0600

    added mission page

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from './pages/home';
 import Calendar from './pages/calendar';
 import Login from './pages/login';
+import Mission from './pages/mission';
 import Chat from './pages/chat';
 import PageNotFound from "./pages/pageNotFound";
 
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/" element={<Home/>} />
                 <Route path="/calendar" element={<Calendar/>} />
                 <Route path="/login" element={<Login/>} />
+                <Route path="/mission" element={<Mission/>} />
                 <Route path="/chat" element={<Chat/>} />
                 <Route path="*" element={<PageNotFound/>} />
             </Routes>
